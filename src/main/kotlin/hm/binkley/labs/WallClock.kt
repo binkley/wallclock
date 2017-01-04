6d5b1533961c8b4608969774699745604c1ce076
Brian Oxley <boxley@thoughtworks.com>
2017-01-04 08:51:27 -0600

    Test for bad input

--- src/main/kotlin/hm/binkley/labs/WallClock.kt
+++ src/main/kotlin/hm/binkley/labs/WallClock.kt
@@ -6,7 +6,7 @@ class WallClock {
     class Minutes {
         operator fun get(minute: Int) = when (minute) {
             in 1..5 -> arrayListOf("five", "past")
-            else -> throw IllegalArgumentException()
+            else -> throw IllegalArgumentException("Must be 0-59: $minute")
         }
     }
 }
--- src/test/kotlin/hm/binkley/labs/WallClockTest.kt
+++ src/test/kotlin/hm/binkley/labs/WallClockTest.kt
@@ -1,6 +1,7 @@
 package hm.binkley.labs
 
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertThrows
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
@@ -10,6 +11,13 @@ internal class WallClockTest {
     @BeforeEach
     fun setUpClock() {
         wallClock = WallClock()
+    }
+
+    @Test
+    fun wrongMinutes() {
+        assertThrows<Exception>(IllegalArgumentException::class.java) {
+            wallClock.minutes[-1]
+        }
     }
 
     @Test
